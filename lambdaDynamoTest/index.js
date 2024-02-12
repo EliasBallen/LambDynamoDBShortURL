e8d0181c50f4b10d4a0395bc3323e74f3cbb7ea7
EliasBallen <@EliasBallen>
2024-02-12 08:51:47 -0500

    first commit to the short url Lambda, stablishing test for dynamoDb and IAM roles test

---
+++ lambdaDynamoTest/index.js
@@ -0,0 +1,26 @@
+const AWS  = require('aws-sdk');
+AWS.config.update({ region: process.env.AWS_REGION}); // Actualiza la región si es necesario
+const dynamodb = new AWS.DynamoDB();
+
+exports.handler = async (event) => {
+    try {
+        const params = {
+            TableName: process.env.TABLE_NAME, // Nombre de la tabla
+            Key: {
+                'user_id': { S: 'alice456' } // Valor de la clave de partición
+            }
+        };
+        // Realiza la operación de lectura de un usuario
+        const result = await dynamodb.getItem(params).promise();
+        return result.Item;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
+//DynamoDB_CRUD_FOR_prefix-lbproj1
+
+
+
+
+
